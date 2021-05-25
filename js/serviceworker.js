eb9220de3912821a67dba0410b0ee58d4768832d
Marc Schraffenberger <marc@schraffenberger.com>
2021-05-25 09:23:27 -0400

    initial ci

---
+++ js/serviceworker.js
@@ -0,0 +1,48 @@
+const version = 'template-v1'
+const assets = [
+  'index.html',
+  'style.css',
+  'js/main.js',
+  'js/serviceworker.js',
+  'images/icon-180.png',
+  'images/icon-192.png',
+  'images/icon-512.png'
+]
+
+async function install() {
+  const cache = await caches.open(version)
+  await cache.addAll(assets)  
+}
+
+async function handle(request) {
+  const cached = await caches.match(request)
+  
+  if (cached) {
+    return cached
+  }
+  
+  const response = await fetch(request)
+  const cache = await caches.open(version)
+  
+  cache.put(request, response.clone())
+  return response
+}
+
+self.addEventListener('install', e => {
+  e.waitUntil(install())
+})
+
+self.addEventListener('fetch', e => {
+  const request = e.request
+  e.respondWith(handle(request))
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => {
+    Promise.all(keys.map(key => {
+      if (key !== version) {
+        caches.delete(key)
+      }
+    }))
+  })
+})
